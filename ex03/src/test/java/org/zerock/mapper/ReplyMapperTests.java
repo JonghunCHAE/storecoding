74fe97b776086722b19ab379d062159f391a2d32
chaejonghun <ace1373@naver.com>
2020-12-30 23:48:37 +0900

    387페이지 상단까지 완료

---
+++ ex03/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -0,0 +1,81 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.ReplyVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReplyMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	//@Test
+	public void testMapper() {
+		assertNotNull(mapper);
+		log.info(mapper);
+	}
+	
+	//테스트 전에 해당 글 번호가 존재하는지 체크
+	private Long[] bnoArr = {264L, 263L, 262L, 261L, 260L};
+	
+	//@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			ReplyVO vo = new ReplyVO();
+			
+			//게시물의 번호
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글 테스트 " + i);
+			vo.setReplyer("replyer" + i);
+			
+			mapper.insert(vo);
+		});
+	}
+	
+	//@Test
+	public void testRead() {
+		
+		Long targetRno = 5L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		log.info(vo);
+	}
+	
+	//@Test
+	public void testDelete() {
+		
+		Long targetRno = 1L;
+		
+		mapper.delete(targetRno);
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		Long targetRno = 10L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		vo.setReply("Update Reply ");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: " + count);
+	}
+	
+	
+}
